429a9d4e2b8e71959fc45cc958940002d77b84a6
SkhundinMichail <113530482+SkhundinMichail@users.noreply.github.com>
2023-05-25 11:57:38 +0300

    544235ytgfg

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -128,7 +128,9 @@ const loginUser = (req, res, next) => {
   return UserSchema.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key', { expiresIn: '7d' });
-      res.cookie('jwt', token, { maxAge: 10000000000, httpOnly: true }).send({ message: 'goodBoy' });
+      res.cookie('jwt', token, {
+        sameSite: 'none', secure: true, maxAge: 10000000000, httpOnly: true,
+      }).send({ message: 'goodBoy' });
     })
     .catch(next);
 };
